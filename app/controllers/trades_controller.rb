9f57af99e0f9238125b178a3e9da2c74213a1140
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-08-02 07:06:40 +0000

    added edit, update, index, destroy actions; updated show action

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,4 +1,9 @@
 class TradesController < ApplicationController
+  
+  def index
+    @trades = Trade.all
+  end
+  
   def new
     @trade = Trade.new
   end
@@ -6,15 +11,36 @@ class TradesController < ApplicationController
   def create
     @trade = Trade.new(trade_params)
     if @trade.save
-      flash[:notice] = "Your Trade has been setup, It will show on your trades page"
+      flash[:notice] = "Your Trade has been setup, It will show on your trades page."
       redirect_to trade_path(@trade)
     else
       render 'new'
     end
   end
   
+  def edit
+    @trade = Trade.find(params[:id])
+  end
+  
+  def update
+    @trade = Trade.find(params[:id])
+    if @trade.update(trade_params)
+      flash[:notice] = "Your Trade has been edited."
+      redirect_to trade_path(@trade)
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @trade = Trade.find(params[:id])
+  end
+  
+  def destroy
+    @trade = Trade.find(params[:id])
+    @trade.destroy
+    flash[:notice] = "Trade was successfully deleted!"
+    redirect_to trades_path
   end
   
   private
